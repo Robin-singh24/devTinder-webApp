05397901e544c2fbc70ba63473687028b0f2a3b4
Robin-singh24 <robin.4github@gmail.com>
2025-11-28 06:33:37 +0530

    Footer and some routes

---
+++ src/Body.jsx
@@ -0,0 +1,15 @@
+import Footer from './Footer.jsx';
+import NavBar from './NavBar.jsx';
+import { Outlet } from 'react-router-dom';
+
+const Body = () => {
+  return (
+    <div>
+        <NavBar />
+        <Outlet />
+        <Footer />
+    </div>
+  )
+}
+
+export default Body
---
+++ src/Profile.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <div><h1>Your Profile <b>BITCH!!!!!!</b></h1></div>
+  )
+}
+
+export default Profile
