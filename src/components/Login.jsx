4d7ebdce07eeb12f004e5ffa45f6529cdc8742f8
Robin-singh24 <robin.4github@gmail.com>
2025-12-04 12:50:53 +0530

    feat: Signup and minor e2e testing

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -10,6 +10,7 @@ const Login = () => {
     const [lastName, setLastName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [isLoginForm, setIsLoginForm] = useState(false);
     const [error, setError] = useState("");
 
     const dispatch = useDispatch();
@@ -29,13 +30,29 @@ const Login = () => {
         }
     }
 
+    const handleSignUp = async() => {
+        try {
+            const res = await axios.post(BASE_URL + "/signup",{
+                firstName,
+                lastName,
+                email,
+                password,
+            }, {withCredentials:true});
+            dispatch(addUser(res.data.data));
+            return navigate("/profile");
+        } catch (error) {
+            setError(error?.response?.data || "Something went wrong!!!");
+        }
+    }
+
     return (
         <div>
-            <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-full max-w-md border p-4 mx-auto mt-10">
+            <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-full 
+            max-w-md border p-4 mx-auto mt-10 ">
 
-                <h2 className="text-3xl font-semibold text-center mb-4">Login</h2>
+                <h2 className="text-3xl font-semibold text-center mb-4">{isLoginForm ? "Login" : "Sign Up"}</h2>
 
-                <div>
+                {!isLoginForm && (<><div>
                     <label className="label text-xl">First Name</label>
                     <input
                         type="text"
@@ -45,15 +62,15 @@ const Login = () => {
                     />
                 </div>
 
-                <div className="mt-3">
-                    <label className="label text-xl">Last Name</label>
-                    <input
-                        type="text"
-                        value={lastName}
-                        className="input w-full"
-                        onChange={(e) => setLastName(e.target.value)}
-                    />
-                </div>
+                    <div className="mt-3">
+                        <label className="label text-xl">Last Name</label>
+                        <input
+                            type="text"
+                            value={lastName}
+                            className="input w-full"
+                            onChange={(e) => setLastName(e.target.value)}
+                        />
+                    </div></>)}
                 <div>
                     <label className="label text-xl">Email</label>
                     <input
@@ -75,10 +92,14 @@ const Login = () => {
                 </div>
                 <p className='text-red-800 flex justify-center text-sm'>{error}</p>
                 <div className="flex justify-center mt-4">
-                    <button className="btn btn-neutral" onClick={handleLogin}>
-                        Login
+                    <button className="btn btn-neutral" onClick={isLoginForm ? handleLogin : handleSignUp}>
+                        {isLoginForm ? "Login" : "Sign Up"}
                     </button>
                 </div>
+
+                <p className='m-auto cursor-pointer py-2' onClick={()=>setIsLoginForm((value) => !value)}>
+                    {isLoginForm ? "New User? Click here to Sign Up" : "Existing User? Click here to Login"}
+                </p>
 
             </fieldset>
         </div>
